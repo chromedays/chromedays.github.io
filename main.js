35050df2ea519cc20d6c78acd63171cb52d4f2cf
Illkwon Ha <haik0925@gmail.com>
2020-01-19 05:03:50 -0800

    added brief aboutme

--- main.js
+++ main.js
@@ -72,14 +72,32 @@ customElements.define("menu-bar", MenuBar);
 class ContentsContainer extends HTMLElement {
   constructor() {
     super();
+    this.contentsType = "about";
     this.render();
   }
 
   render() {
-    this.innerHTML = `
+    this.innerHTML = "";
+    switch (this.contentsType) {
+      case "blog":
+        this.innerHTML = `<div>N/A</div>`;
+        break;
+      case "about":
+        this.innerHTML = `
         <div>
+        My name is Ilgwon. I am a junior student majoring in BS in CS in Real-Time Interactive Simulation at DigiPen Institute of Technology.
         </div>
     `;
+        break;
+      case "projects":
+        this.innerHTML = `<div>N/A</div>`;
+        break;
+    }
+  }
+
+  setContentsType(contentsType) {
+    this.contentsType = contentsType;
+    this.render();
   }
 }
 customElements.define("contents-container", ContentsContainer);
@@ -88,9 +106,9 @@ class MainApp extends HTMLElement {
   constructor() {
     super();
 
-    this.contentType = "projects";
-
     this.render();
+
+    this.contents = this.querySelector("contents-container");
   }
 
   render() {
@@ -111,25 +129,27 @@ class MainApp extends HTMLElement {
     menuBar.addItem(
       new MenuItem("blog", () => {
         console.log("blog");
-        this.contentType = "blog";
-        this.render();
+        this.contents.setContentsType("blog");
       })
     );
     menuBar.addItem(
       new MenuItem("about", () => {
         console.log("about");
-        this.contentType = "about";
-        this.render();
+        this.contents.setContentsType("about");
       })
     );
     menuBar.addItem(
       new MenuItem("projects", () => {
         console.log("projects");
-        this.contentType = "projects";
-        this.render();
+        this.contents.setContentsType("projects");
       })
     );
-    menuBar.addItem(new DirectLinkItem("resume", "http://bit.ly/2NFEZdQ"));
+    menuBar.addItem(
+      new DirectLinkItem(
+        "resume",
+        "https://docs.google.com/viewer?url=https://github.com/chromedays/chromedays.github.io/raw/master/resume.pdf"
+      )
+    );
   }
 }
 customElements.define("main-app", MainApp);
